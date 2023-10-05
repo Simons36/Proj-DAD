59d81d3bc530b1a28bab694f5096294bd92588be
Miguel Pato <migu3l.pach3co@gmail.com>
2023-10-06 00:43:13 +0100

    Not much, good luck simão

--- LeaseManager/LeaseManager.cs
+++ LeaseManager/LeaseManager.cs
@@ -1,20 +1,72 @@
-﻿namespace LeaseManager{
+﻿namespace LeaseManager
+{
     public class LeaseManager
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World! leaseManager");
-            foreach(string arg in args)
+            string name = "";
+            int timeslotNumber = 0,
+                duration = 0;
+            TimeOnly startingTime = new TimeOnly();
+            List<string> leaseManagerUrls = new List<string>();
+
+            // Extract and process the arguments
+            for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine(arg);
+                string arg = args[i];
+
+                switch (arg)
+                {
+                    case "-n":
+                        name = args[i + 1];
+                        break;
+
+                    case "-nr":
+                        timeslotNumber = int.Parse(args[i + 1]);
+                        break;
+
+                    case "-d":
+                        duration = int.Parse(args[i + 1]);
+                        break;
+
+                    case "-t":
+                        startingTime = TimeOnly.Parse(args[i + 1]);
+                        break;
+
+                    case "-nl":
+                        for (int k = i + 1; k < args.Length; k++)
+                        {
+                            string nextArg = args[k];
+                            if (nextArg.StartsWith("http://"))
+                            {
+                                leaseManagerUrls.Add(nextArg);
+                            }
+                            else
+                            {
+                                // Break if the next argument does not start with "http://"
+                                break;
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            // Display the extracted values for verification
+            Console.WriteLine("\nExtracted values:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Timeslot Number: " + timeslotNumber);
+            Console.WriteLine("Duration: " + duration);
+            Console.WriteLine("Starting Time: " + startingTime);
+            Console.WriteLine("Lease Manager URLs:");
+            foreach (var url in leaseManagerUrls)
+            {
+                Console.WriteLine(url);
             }
             Console.ReadLine();
         }
-
-        public void Run(string[] args)
-        {
-            Main(args);
-        }
     }
 }
-
