8a415a9fe9c8f3deeda69395ae29b0d5f93a4ee6
Simão Silva <email@simaosilva.com>
2023-10-06 15:00:53 +0100

    created server state in tm

---
+++ TransactionManager/src/state/ServerState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionManager.src.state
+{
+    public class ServerState
+    {
+        
+    }
+}
